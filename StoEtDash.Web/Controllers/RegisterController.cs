b48de78c843d2c0e0a0da18c27d5f2c411403a94
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2023-03-07 23:39:56 +0100

    Added design of login and register

---
+++ StoEtDash.Web/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoEtDash.Web.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

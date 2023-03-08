b9328765477028f84d1c055f3733b486c760609a
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2023-03-08 23:53:41 +0100

    Created user database

---
+++ StoEtDash.Web/Database/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoEtDash.Web.Database.Models
+{
+    public class User
+    {
+        [Key]
+        [Column(TypeName = "VARCHAR (60)")]
+        public string Username { get; set; } = string.Empty;
+
+        [Column(TypeName = "VARCHAR (128)")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ StoEtDash.Web/Migrations/20230308221053_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoEtDash.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "VARCHAR (60)", nullable: false),
+                    Password = table.Column<string>(type: "VARCHAR (128)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Username);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

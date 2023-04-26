393d6f2539c4bfd7f66e79e04845f1cdd8f25531
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2023-04-26 15:36:38 +0200

    Fixed table stripping aster filter update

--- StoEtDash.Web/wwwroot/js/dashboard.js
+++ StoEtDash.Web/wwwroot/js/dashboard.js
@@ -5,33 +5,33 @@ const setDataInSellTransactionModal = (name, ticker) => {
 }
 
 const onAssetClick = (e, ticker) => {
-    // Do not count as click on asset if button with sell was clicked
-    if (e.target.tagName == 'BUTTON') {
-        return;
-    }
+	// Do not count as click on asset if button with sell was clicked
+	if (e.target.tagName == 'BUTTON') {
+		return;
+	}
 
-    $.ajax({
-        type: "POST",
-        url: "/Dashboard/ShowAssetTransactions?ticker=" + ticker,
-        contentType: "application/json; charset=utf-8",
-        dataType: "html",
-        success: function (response) {
-            $("#transaction-list-modal").find(".modal-body").html(response);
-            $("#transaction-list-modal").modal('show');
-        }
-    });
+	$.ajax({
+		type: "POST",
+		url: "/Dashboard/ShowAssetTransactions?ticker=" + ticker,
+		contentType: "application/json; charset=utf-8",
+		dataType: "html",
+		success: function (response) {
+			$("#transaction-list-modal").find(".modal-body").html(response);
+			$("#transaction-list-modal").modal('show');
+		}
+	});
 }
 
 // _Transaction.cshtml
 const onEditTransactionClick = (transactionId) => {
-    $.ajax({
-        type: "POST",
-        url: "/Dashboard/ShowEditTransactionModal?transactionId=" + transactionId,
-        contentType: "application/json; charset=utf-8",
-        dataType: "html",
-        success: function (response) {
-            $("#edit-transaction-modal").find(".modal-body").html(response);
-            $("#edit-transaction-modal").modal('show');
-        }
-    });
+	$.ajax({
+		type: "POST",
+		url: "/Dashboard/ShowEditTransactionModal?transactionId=" + transactionId,
+		contentType: "application/json; charset=utf-8",
+		dataType: "html",
+		success: function (response) {
+			$("#edit-transaction-modal").find(".modal-body").html(response);
+			$("#edit-transaction-modal").modal('show');
+		}
+	});
 }
